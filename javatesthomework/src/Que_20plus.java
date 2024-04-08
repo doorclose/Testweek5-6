73d9c17e3efdcac1f3d050c42b76b4385ab1556d
doorclose <codebuster2023@gmail.com>
2024-04-08 22:46:20 +0100

    My first push to repo.

---
+++ javatesthomework/src/Que_20plus.java
@@ -0,0 +1,15 @@
+public class Que_20plus {
+
+
+//Write a program to add 5 and 6.2, and print in console..//
+
+    public static void main(String[] args) {
+        double num1 = 5;
+        double num2 = 6.2;
+
+        double sum = num1 + num2 ;
+
+        System.out.println(" Addition Result::-> 5 + 6.2 = "  + sum );
+
+    }
+}
