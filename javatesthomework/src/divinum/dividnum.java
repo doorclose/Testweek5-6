95ca8dc1052d5df01f7e4ec4242169540eafea30
doorclose <codebuster2023@gmail.com>
2024-04-08 10:25:09 +0100

    my first push to repo.

---
+++ javatesthomework/src/divinum/dividnum.java
@@ -0,0 +1,21 @@
+package divinum;
+
+import javax.sound.midi.Soundbank;
+
+public class dividnum {
+
+    public static void main(String[] args) {
+
+        //divide two numbers and print on the screen. 50/3//
+
+        int a = 50;
+        int b = 3;
+
+        int c = 50/3;
+
+        //Output of ***** 50/3 ****//
+
+        System.out.println("Expected Output:->>" +c);
+
+    }
+}
