c4501e9a867057d63688136da338798e4f21757d
doorclose <codebuster2023@gmail.com>
2024-04-08 10:46:22 +0100

    my second push to repo.

---
+++ javatesthomework/src/mutipltwonumber/Twonumprodu.java
@@ -0,0 +1,16 @@
+package mutipltwonumber;
+//Multiply two number ****** 25*5=125 *****//
+public class Twonumprodu {
+    public static void main(String[] args) {
+        int x =25;
+        int y = 5;
+
+
+        int Result = x * y;
+
+        //Mutiplication
+        System.out.println("expected output:-->" + Result);
+    }
+
+
+}
