641bf3ec77a1494290d288e58b3aba8fe55aaf1c
doorclose <codebuster2023@gmail.com>
2024-04-08 22:33:56 +0100

    My first push to repo.

---
+++ javatesthomework/src/Que_16testope.java
@@ -0,0 +1,10 @@
+public class Que_16testope {
+
+    public static void main(String[] args) {
+        //Test Data: ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))//
+
+
+        double result =  ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println("Expected Output:-->>  " +result);
+    }
+}
