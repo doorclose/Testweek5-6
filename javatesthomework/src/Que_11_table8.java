40e43f9fa4ddbde98d219799eed6130ecd1f61a4
doorclose <codebuster2023@gmail.com>
2024-04-08 22:23:05 +0100

    My first push to repo.

---
+++ javatesthomework/src/Que_11_table8.java
@@ -0,0 +1,10 @@
+public class Que_11_table8 {
+    public static void main(String[] args) {
+
+            for (int i = 1; i < 11; i++) {
+            int result = 8 * i;
+                System.out.println("8*"+i+"="+result);
+            }
+
+    }
+}
