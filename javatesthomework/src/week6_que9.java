92c1224034f61d1dca2064db7a6aee82a5e1d9f0
doorclose <codebuster2023@gmail.com>
2024-04-10 23:53:38 +0100

    My first push to repo.

---
+++ javatesthomework/src/week6_que9.java
@@ -0,0 +1,8 @@
+public class week6_que9 {
+
+    public static void main(String[] args) {
+        String text = "BEAUTIFUL WORLD";
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+    }
+}
