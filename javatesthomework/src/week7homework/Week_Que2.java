ff73aa49c26976ced1fa3224e5f033f5cc9ddf59
doorclose <codebuster2023@gmail.com>
2024-04-14 23:08:17 +0100

    My first push to repo

---
+++ javatesthomework/src/week7homework/Week_Que2.java
@@ -0,0 +1,10 @@
+package week7homework;
+//2. Declare array and store any 5 countries and print them in console.
+public class Week_Que2 {
+
+    public static void main(String[] args) {
+        String[] Countries = {"  India,Japan,Rom,U.K,Canada  "};
+        System.out.println(Countries[0]);
+
+    }
+}
