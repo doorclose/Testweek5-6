4c640b64104ac94ee1aa590d998a84858cf437cb
doorclose <codebuster2023@gmail.com>
2024-04-15 21:33:34 +0100

    My first push to repo

---
+++ javatesthomework/src/week7homework/Wwek7_Que4.java
@@ -0,0 +1,18 @@
+package week7homework;
+//Write if else condition and print your group name in console else others group name.
+public class Wwek7_Que4 {
+    public static void main(String[] args) {
+        String MyGroupName = "Rajasthan Roayal";
+        String OtherGroupName = " Mumbai Indians";
+
+
+        if ( (MyGroupName) ==(OtherGroupName) ){
+            System.out.println("This is not my  group" + OtherGroupName);
+        } else {
+            System.out.println("This is  my lovely group  " + MyGroupName);
+        }
+
+
+    }
+
+}
