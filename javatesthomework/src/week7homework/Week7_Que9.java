f2e7cab7e02d08ff6b389b25eaec2598884bb707
doorclose <codebuster2023@gmail.com>
2024-04-15 22:24:16 +0100

    My first push to repo

---
+++ javatesthomework/src/week7homework/Week7_Que9.java
@@ -0,0 +1,14 @@
+package week7homework;
+
+public class Week7_Que9 {
+
+    public static void main(String[] args) {
+        String name = "Software girls";
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i );
+            //System.out.println(" Software girls ");
+            System.out.println(" " + name);
+
+        }
+    }
+}
