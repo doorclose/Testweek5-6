ee183c4686f8943658cdf92e71db9a94b8df0596
doorclose <codebuster2023@gmail.com>
2024-04-17 00:03:05 +0100

    My first push to repo.

---
+++ javatesthomework/src/week7homework/Week7_Que21.java
@@ -0,0 +1,15 @@
+package week7homework;
+//Write a Java program to sum values of an array.
+public class Week7_Que21 {
+
+    public static void main(String[] args) {
+        int[] number = {10, 21, 32, 43, 56};
+        int sum = 0;
+        int i;
+       for (i = 0; i < number.length; i++){
+            sum = sum + number[i];
+
+       }
+        System.out.println("Sum of all array is :->" +sum);
+    }
+}
