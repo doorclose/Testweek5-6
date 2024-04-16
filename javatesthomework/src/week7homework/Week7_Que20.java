2a5dd6f8b3e6575d40d46fd130988806b9f1bf74
doorclose <codebuster2023@gmail.com>
2024-04-16 23:38:50 +0100

    My first push to repo.

---
+++ javatesthomework/src/week7homework/Week7_Que20.java
@@ -0,0 +1,11 @@
+package week7homework;
+//Write a Java program to sort a numeric array and a string array.
+public class Week7_Que20 {
+    public static void main(String[] args) {
+        int[] number = {11, 22, 33, 44, 55};
+        String[] name = {"John", "Lucy", "May", "David"};
+        System.out.println(number[3]);
+        System.out.println(name[2]);
+
+    }
+}
