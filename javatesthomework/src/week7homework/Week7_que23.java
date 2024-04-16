44cfc4b55ee8e98b692b915c0db8d91923ae356c
doorclose <codebuster2023@gmail.com>
2024-04-17 00:11:03 +0100

    My first push to repo.

---
+++ javatesthomework/src/week7homework/Week7_que23.java
@@ -0,0 +1,12 @@
+package week7homework;
+//Write java program using for loop and print your name 10 times.
+public class Week7_que23 {
+    public static void main(String[] args) {
+        String name = "Kinjal";
+        for(int i=0; i<10;i++){
+
+            System.out.println(name);
+        }
+    }
+
+}
