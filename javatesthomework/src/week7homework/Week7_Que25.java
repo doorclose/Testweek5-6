983139fa8ee4646c91bdfe102919b9e2fb8c5271
doorclose <codebuster2023@gmail.com>
2024-04-17 21:55:48 +0100

    My first push to repo.

---
+++ javatesthomework/src/week7homework/Week7_Que25.java
@@ -0,0 +1,16 @@
+package week7homework;
+//25 Declare one method with return type and parameter and print your name in console.
+public class Week7_Que25 {
+
+    public static void main(String[] args) {
+     String name = "Kinjal";
+     printName(name);
+    }
+
+    public static void printName(String name) {
+        System.out.println(" My name is " + name);
+    }
+
+
+}
+
