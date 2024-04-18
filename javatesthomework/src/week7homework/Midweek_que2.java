470480503ecaae57e8390be301c10b0a47a25c65
doorclose <codebuster2023@gmail.com>
2024-04-18 21:24:45 +0100

    My first push to repo.

---
+++ javatesthomework/src/week7homework/Midweek_que2.java
@@ -0,0 +1,32 @@
+package week7homework;
+
+public class Midweek_que2 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("table of 24 :->" );
+        System.out.println("____________");
+        for (int i = 1; i < 11; i++) {
+            int result = 24 * i;
+            System.out.println("24 * " + i + "=  " + result);
+        }
+        System.out.println();
+        System.out.println("table of 50 :->");
+        System.out.println("__________-");
+        for (int n = 1; n < 11; n++) {
+                int result1 = 50 * n;
+                System.out.println("50 * " + n + "=  " + result1);
+
+            }
+        System.out.println();
+        System.out.println("table of 29:->");
+        System.out.println("--------------");
+        for (int m = 1; m < 11; m++) {
+            int result1 = 29 * m;
+            System.out.println("29 * " + m + "=  " + result1);
+
+        }
+    }
+}
+
