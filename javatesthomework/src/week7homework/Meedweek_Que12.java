6278bce86c52237f468d75915797731013761353
doorclose <codebuster2023@gmail.com>
2024-04-18 23:15:30 +0100

    My first push to repo.

---
+++ javatesthomework/src/week7homework/Meedweek_Que12.java
@@ -0,0 +1,9 @@
+package week7homework;
+//Write a program to find the length of the string "refrigerator".
+public class Meedweek_Que12 {
+    public static void main(String[] args) {
+        String txt = "refrigerator";
+
+        System.out.println("Lenght of the string is :->" + txt.length());
+    }
+}
