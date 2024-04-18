fe548a92d3f3caa73ed498b8f305a56b375d5f51
doorclose <codebuster2023@gmail.com>
2024-04-18 23:08:49 +0100

    My first push to repo.

---
+++ javatesthomework/src/week7homework/Meedweek_Que11.java
@@ -0,0 +1,19 @@
+package week7homework;
+
+import java.util.Scanner;
+//Write a program to print a string entered by user.
+public class Meedweek_Que11 {
+
+
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter a string: ");
+            String givenString = scan.nextLine();
+
+            System.out.println("Please enter your string   ==>  " + givenString);
+
+
+        }
+    }
+
